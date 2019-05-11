bfb06a32a67f1b7f6e321f2202dc8140cc5053ac
kaloyan <kaloyan.katsarov@gmail.com>
2019-05-11 20:39:52 +0300

    CSCB869 setting up readme

--- src/main/java/bulkcode/service/mapper/CompanyMapper.java
+++ src/main/java/bulkcode/service/mapper/CompanyMapper.java
@@ -3,19 +3,12 @@ package bulkcode.service.mapper;
 import bulkcode.domain.*;
 import bulkcode.service.dto.CompanyDTO;
 
-import org.mapstruct.*;
-import org.mapstruct.factory.Mappers;
 
 /**
  * Mapper for the entity {@link Company} and its DTO {@link CompanyDTO}.
  */
-@Mapper(componentModel = "spring", uses = {})
 public interface CompanyMapper extends EntityMapper<CompanyDTO, Company> {
 
-//	public CompanyMapper INSTANCE = Mappers.getMapper(CompanyMapper.class);
-
-//    @Mapping(source = "numberOfSeats", target = "seatCount")
-//    CompanyDTO toDto(Company company); 
 
 	default Company fromId(Long id) {
 		if (id == null) {
